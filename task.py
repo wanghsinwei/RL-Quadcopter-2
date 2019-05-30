9c23dfc815440937d0734ce95dbbd1a63b9c2164
Hsin-Wei Wang <wanghsinwei@msn.com>
2019-05-31 03:25:45 +0800

    Add DDPG agent but the performance is still very poor.

--- task.py
+++ task.py
@@ -1,7 +1,76 @@
 import numpy as np
 from physics_sim import PhysicsSim
 
-class Task():
+class MyTask:
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., target_pos=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 12 # 6 degrees of freedom + 3 velocity + 3 angular velocity
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4 # 4 rotors
+
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
+
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        dist = np.linalg.norm(self.sim.pose[:3] - self.target_pos)
+        reward = self.sim.v[2] - dist - abs(self.sim.pose[2] - self.target_pos[2])
+
+        # if self.sim.pose[3] >= np.pi/3:
+        #     reward -= 10
+
+        # if self.sim.pose[4] >= np.pi/3:
+        #     reward -= 10
+        
+        # if dist <= 0.1:
+        #     reward += 100
+
+        # if abs(self.sim.pose[2] - self.target_pos[2]) <= 1:
+        #     reward += 50
+
+        # if reward > 0:
+        #     reward = 1
+        # elif reward < 0:
+        #     reward = -1
+
+        return np.tanh(reward)
+        # return reward
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward() 
+            pose_all.append(self.sim.pose)
+            pose_all.append(self.sim.v)
+            pose_all.append(self.sim.angular_v)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose, self.sim.v, self.sim.angular_v] * self.action_repeat)
+        return state
+
+class Task:
     """Task (environment) that defines the goal and provides feedback to the agent."""
     def __init__(self, init_pose=None, init_velocities=None, 
         init_angle_velocities=None, runtime=5., target_pos=None):
