29593ca5db59736df7fdaa8e00c81782a322427d
Hsin-Wei Wang <wanghsinwei@msn.com>
2019-06-07 17:39:24 +0800

    Fix action repeat of ReachTargetTask

--- tasks/target_task.py
+++ tasks/target_task.py
@@ -15,7 +15,7 @@ class ReachTargetTask:
         """
         # Simulation
         self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
-        self.action_repeat = 1
+        self.action_repeat = 3
 
         self.state_size = self.action_repeat * 12 # 3 Euler angles + 3 velocity + 3 angle velocity + target vector
         self.action_low = 1 # Use a value larger than 0 to avoid the error of divide by zero
@@ -24,7 +24,6 @@ class ReachTargetTask:
 
         # Goal
         self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
-        self.success = False
         self.goal_dist = 0.5
         self.last_target_vec_abs = np.absolute(self.target_pos - self.sim.pose[:3])
 
@@ -33,8 +32,7 @@ class ReachTargetTask:
     def get_reward(self):
         """Uses current pose of sim to return reward."""
         target_vec = self.target_pos - self.sim.pose[:3]
-        
-        # dist = np.linalg.norm(target_vec)
+        dist = np.linalg.norm(target_vec)
         # target_unit_vec = target_vec / dist
         # v_proj_target = np.inner(self.sim.v, target_unit_vec) * target_unit_vec
         # reward = 0.1*v_proj_target[2] + 0.01*(v_proj_target[0] + v_proj_target[1])
@@ -57,7 +55,7 @@ class ReachTargetTask:
         if self.sim.pose[2] > 0:
             reward += 0.1
 
-        if self.success:
+        if dist <= self.goal_dist:
             reward = 20 # Reach Goal
         elif self.sim.done:
             if not self.has_takeoff:
@@ -73,22 +71,18 @@ class ReachTargetTask:
         """Uses action to obtain next state, reward, done."""
         reward = 0
         state_all = []
-        for _ in range(self.action_repeat):
+        for i in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
             if not self.has_takeoff and self.sim.pose[2] > 0:
                 self.has_takeoff = True
             target_vec = self.target_pos - self.sim.pose[:3]
-            dist = np.linalg.norm(target_vec)
-            if not done and dist <= self.goal_dist:
+            if not done and np.linalg.norm(target_vec) <= self.goal_dist: # Reach Goal
                 done = True
-                self.success = True
             reward += self.get_reward() 
             state_all.append(self.sim.pose[3:])
             state_all.append(self.sim.v)
             state_all.append(self.sim.angular_v)
             state_all.append(target_vec)
-            if done:
-                break
         next_state = np.concatenate(state_all)
 
         return next_state, reward, done
@@ -100,7 +94,6 @@ class ReachTargetTask:
         target_vec = self.target_pos - self.sim.pose[:3]
         self.last_target_vec_abs = np.absolute(target_vec)
         state = np.concatenate([self.sim.pose[3:], self.sim.v, self.sim.angular_v, target_vec] * self.action_repeat)
-        self.success = False
         return state
 
     def sample_act(self):
